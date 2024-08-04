b8ea34966a7cd1d63c326f62ac888ced3a1b7482
David Albasini <72087495+Shellv1@users.noreply.github.com>
2024-08-04 18:23:28 -0500

    Added Top Interview 150 (1005).java

---
+++ Top Interview 150/Top Interview 150 (1005).java
@@ -0,0 +1,45 @@
+// Runtime: 1ms     (Beats 99.79%)
+// Memory:  52.96MB (Beats 47.03)
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        // An array with less than 3 elements will have no majority
+        int iLen = nums.length;
+        if (iLen < 3) { return nums[0]; }
+
+        // Variable Declarations
+        int iTemp = 0;
+        int iCount = 0;
+        int iElement = 0;
+        int j = 0;
+
+        // Iterate through array, exit if current element is the majority
+        for (int i=0; (i < iLen) && (iCount < (iLen/2)+1); i++) {
+            // Reset count if starting a new element
+            if (iElement != nums[i]) {
+                iCount = 1;
+                iElement = nums[i];
+            }
+            // Increment count if continuing from same element
+            else { iCount++; }
+
+            // From the end of the array, if the element is the same:
+            for (int j = iLen-1; (j > i); j--) {
+                if (nums[j] == iElement) {
+                    // Move i to next non-matching element
+                    while ((nums[i] == iElement) && (i < j)) {
+                        i++;
+                        iCount++;
+                    }
+                    // Swap i and j
+                    iTemp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = iTemp;
+                }
+            }
+        }
+
+        // Return majority element
+        return iElement;
+    }
+}
