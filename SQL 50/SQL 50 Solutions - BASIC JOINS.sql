51f1770012cec4d3e38202a0f551ce15d55c3b3e
David Albasini <72087495+Shellv1@users.noreply.github.com>
2025-02-02 20:23:38 -0600

    Update SQL 50 Solutions - BASIC JOINS.sql

--- SQL 50/SQL 50 Solutions - BASIC JOINS.sql
+++ SQL 50/SQL 50 Solutions - BASIC JOINS.sql
@@ -79,3 +79,20 @@ WHERE
 # = = = = = = = = = = = = = = = #
 
 # PROBLEM 12
+SELECT
+    stu.student_id,
+    stu.student_name,
+    sub.subject_name,
+    COUNT(exam.subject_name) attended_exams
+FROM Students stu
+    CROSS JOIN Subjects sub
+    LEFT JOIN Examinations exam
+    ON
+        stu.student_id = exam.student_id AND
+        sub.subject_name = exam.subject_name
+GROUP BY student_id, subject_name
+ORDER BY student_id, subject_name
+
+# = = = = = = = = = = = = = = = #
+
+# PROBLEM 13
