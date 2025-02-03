e5ac7d5ae17f69a688e945ceb56961f59716b69b
David Albasini <72087495+Shellv1@users.noreply.github.com>
2025-02-02 18:44:30 -0600

    Update and rename SQL 50 Solutions.sql to SQL 50 Solutions - SELECT.sql

---
+++ SQL 50/SQL 50 Solutions - SELECT.sql
@@ -0,0 +1,42 @@
+# = = = = = = = = = = = = = = = #
+#   SELECT                      #
+# = = = = = = = = = = = = = = = #
+
+# PROBLEM 01
+SELECT product_id
+FROM Products
+WHERE
+    low_fats = 'Y' AND
+    recyclable = 'Y';
+
+# = = = = = = = = = = = = = = = #
+
+# PROBLEM 02
+SELECT name
+FROM Customer
+WHERE (referee_id <> 2) is not false;
+# OPTION 2 | WHERE COALESCE(referee_id, '') <> 2;
+
+# = = = = = = = = = = = = = = = #
+
+# PROBLEM 03
+SELECT name, population, area
+FROM World
+WHERE
+    (area >= 3000000) OR
+    (population >= 25000000);
+
+# = = = = = = = = = = = = = = = #
+
+# PROBLEM 04
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id;
+
+# = = = = = = = = = = = = = = = #
+
+# PROBLEM 05
+SELECT tweet_id
+FROM Tweets
+WHERE (char_length(content) > 15);
